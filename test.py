87ba287a9e7fd70c719e66841b46da820b99c437
Kunj Dedhia <kdedhia@umass.edu>
2020-04-18 13:03:15 -0400

    clean files

--- test.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-import matplotlib.pyplot as plt
-
-plt.scatter(np.asarray([1, 2, 3, 4]), np.asarray([1, 2, 3, 4]), s = 2)
-plt.xlabel('R 109 at 1.016m')
-plt.ylabel('R 108 at 2.032m')
-plt.savefig('test.png')
