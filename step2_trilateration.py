0d5b8aa8e6bcaaf22d50bf44549f29f5bffc872a
Kunj Dedhia <kdedhia@umass.edu>
2020-04-21 22:04:51 -0400

    fixes and comments

--- step2_trilateration.py
+++ step2_trilateration.py
@@ -2,14 +2,13 @@ from scipy.io import loadmat
 import numpy as np
 import pandas as pd
 import math
-import matplotlib
-matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 
+# Trilateration: returns point of intersection
 def triLat(r1, r2, r3):
-    x1, y1 = 0, 0
-    x2, y2 = 0, 2.032
-    x3, y3 = 1.016, 0
+    x1, y1 = 0, 0 # R 103
+    x2, y2 = 0, 2.032 # R 108
+    x3, y3 = 1.016, 0 # R 109
 
     A = 2*x2 - 2*x1
     B = 2*y2 - 2*y1
@@ -22,12 +21,17 @@ def triLat(r1, r2, r3):
 
     return x, y
 
-
 fileRef = {'time': '/T_stmp.mat', 'bin': 'range_bins.mat', 'mag': 'envNoClutterscans.mat'}
 radarRef = {'103': {'time': 'T_stmp_1033', 'bin': 'Rbin_1033', 'mag': 'envNoClutterscansV_1033'}, 
             '108': {'time': 'T_stmp_103', 'bin': 'Rbin_103', 'mag': 'envNoClutterscansV_103'}, 
             '109': {'time': 'T_stmp_102', 'bin': 'Rbin_102', 'mag': 'envNoClutterscansV_102'}
             }
+groundTruthMap = {'diag': {'x': np.asarray([0.762, 1.98]), 'y': np.asarray([0.9652, 3.4])},
+                    'U': {'x': np.asarray([0.762, 0.762, 1.98, 1.98]), 'y': np.asarray([3.4, 0.9652, 0.9652, 3.4])},
+                    'L': {'x': np.asarray([0.762, 1.98, 1.98]), 'y': np.asarray([3.4, 3.4, 0.9652])},
+                    'gamma': {'x': np.asarray([0.762, 1.98, 1.98]), 'y': np.asarray([0.9652, 0.9652, 3.4])},
+                    'four': {'x': np.asarray([0.762, 0.762, 1.98, 1.98]), 'y': np.asarray([3.4, 2.1844, 2.1844, 0.9652])}
+                }
 
 pre_path = './DataSet/Localization/'
 for participant in ['participant1', 'participant2']:
@@ -37,10 +41,12 @@ for participant in ['participant1', 'participant2']:
         triLatRes = np.asarray([0, 0])
         for radar in ['103', '108', '109']:
             for files in fileRef:
+                # load .mat files
                 radarData[radar][files] = loadmat(pre_path + participant + '/' + radar + '/' + pattern + '/' + fileRef[files])[radarRef[radar][files]]
                 if files == 'time' or files == 'bin':
                     radarData[radar][files] = radarData[radar][files].flatten()
 
+        # time synchronization
         startStamp = max([radarData['103']['time'][0], radarData['108']['time'][0], radarData['109']['time'][0]])
         endStamp = min([radarData['103']['time'][-1], radarData['108']['time'][-1], radarData['109']['time'][-1]])
 
@@ -52,38 +58,45 @@ for participant in ['participant1', 'participant2']:
             radarData[radar]['time'] = radarData[radar]['time'][idx1:idx2+1]
             radarData[radar]['mag'] = radarData[radar]['mag'][idx1:idx2+1, :]
 
+            # resampling to 48Hz
             series = pd.DataFrame(data = radarData[radar]['mag'], index = radarData[radar]['time'])
             series.index = pd.to_datetime(series.index, unit='s')
             series = series.resample('20.8L').mean().bfill(axis ='rows')
 
             timeSyncData[radar] = series.to_numpy()
 
+        # based on the sampling frequency
         window_size = 12
         window_shift = 6
         threshold = 50000
         minIterations = min([np.shape(timeSyncData['103'])[0], np.shape(timeSyncData['108'])[0], np.shape(timeSyncData['109'])[0]])
         iterations = math.floor((minIterations - window_size)/window_shift) + 1
+        # windowing
         for i in range(iterations):
             radar1 = timeSyncData['103'][i*window_shift:(i*window_shift)+window_size, :]
-            binIndices1 = set(np.where(radar1 > threshold)[1])
             radar2 = timeSyncData['108'][i*window_shift:(i*window_shift)+window_size, :]
-            binIndices2 = set(np.where(radar2 > threshold)[1])
             radar3 = timeSyncData['109'][i*window_shift:(i*window_shift)+window_size, :]
-            binIndices3 = set(np.where(radar3 > threshold)[1])
-
-            print('Iteration: ' + str(i))
-            print(str(len(binIndices1)) + ' ' + str(len(binIndices2)) + ' ' + str(len(binIndices3)))
             points = []
-            for i1 in binIndices1:
-                r1 = radarData['103']['bin'][i1]
-                for i2 in binIndices2:
-                    r2 = radarData['108']['bin'][i2]
-                    for i3 in binIndices3:
-                        r3 = radarData['109']['bin'][i3]
-                        intPoint = triLat(r1, r2, r3)
-                        if intPoint[0] > 0 and intPoint[1] > 0:
-                            points.append([round(intPoint[0], 3), round(intPoint[1], 3)])
+            for j in range(window_size):
+                binIndices1 = np.where(radar1[j, :] > threshold)[0]
+                binIndices2 = np.where(radar2[j, :] > threshold)[0]
+                binIndices3 = np.where(radar3[j, :] > threshold)[0]
+
+                if len(binIndices1) <= 0 or len(binIndices2) <= 0 or len(binIndices3) <= 0: 
+                    continue
+                else:
+                    # closest range_bin values corresponding to the magnitude above the threshold
+                    r1 = radarData['103']['bin'][binIndices1[0]]
+                    r2 = radarData['103']['bin'][binIndices2[0]]
+                    r3 = radarData['103']['bin'][binIndices3[0]]
+
+                # trilateration
+                intPoint = triLat(r1, r2, r3)
+                # omitting circles that don't intersect
+                if intPoint[0] > 0 and intPoint[0] < 4.04 and intPoint[1] > 0 and intPoint[1] < 4.04:
+                    points.append([round(intPoint[0], 3), round(intPoint[1], 3)])
             
+            # geometric centroid of points 
             if points:
                 triLatRes = np.vstack((triLatRes, np.asarray(points).mean(axis = 0)))
 
@@ -91,8 +104,17 @@ for participant in ['participant1', 'participant2']:
         print(title)
         print(triLatRes)
 
-        plt.scatter(triLatRes[1:, 0], triLatRes[1:, 1], s = 2)
+        # plotting the points with ground truth
+        plt.scatter(triLatRes[1:, 0], triLatRes[1:, 1], s = 2, label='Localization')
+        plt.scatter(np.asarray([0, 1.016, 0]), np.asarray([0, 0, 2.032]), c ='r', s = 100, label='Radars')
+        radarLabel = ['R 103', 'R 109', ' R \n108']
+        for i, txt in enumerate(radarLabel):
+            plt.annotate(txt, ([0, 1.016, 0][i], [0, 0, 2.032][i]), ([0.1, 0.9, 0.1][i], [0.05, 0.15, 1.9][i]))
+        if pattern in groundTruthMap:
+            plt.plot(groundTruthMap[pattern]['x'], groundTruthMap[pattern]['y'], c='g', label='Ground Truth')
         plt.title(title)
-        plt.xlabel('R 109 at 1.016m')
-        plt.ylabel('R 108 at 2.032m')
-        plt.savefig(participant + ' ' + pattern + '.png')
+        plt.xlim((0, 4.04))
+        plt.ylim((0, 4.04))
+        plt.legend(loc="upper right")
+        plt.savefig('./localization_plots/'+participant + ' ' + pattern + '.png')
+        plt.close()
--- step3_step4_vitalSign.py
+++ step3_step4_vitalSign.py
@@ -7,9 +7,11 @@ import pandas as pd
 
 breathRate = np.zeros((6, 7))
 rCnt, cCnt = -1, -1
-rangeRef = {'Radar 1': [0.8128, 1.8161], 'Radar 2': [1.3716, 3.0861], 'Radar 3': [1.1303, 2.8448]}
+# range_bin value to focus for each eadar
+rangeRef = {'Radar 1': [0.8128, 1.8168], 'Radar 2': [1.3716, 3.0861], 'Radar 3': [1.1303, 2.8448]}
 fileRef = {'time': '/t_stmp.mat', 'bin': 'range_bins.mat', 'mag': 'rawscans.mat'}
 radarRef = {'time': 't_stmp', 'bin': 'range_bins', 'mag': 'rawscans'}
+# signal trim indices
 signalTrimRef = {
                 'Radar 1': {
                 'BR_st1': {'browsing': [20, 40], 'fetal_left': [15, 60], 'fetal_right': [15, 60], 'freefall': [0, 55], 'left_turned': [20, 60], 'right_turned': [10, 60]}, 
@@ -33,20 +35,25 @@ for radar in ['Radar 1', 'Radar 2', 'Radar 3']:
             cCnt += 1
             radarData = {}
             for files in fileRef:
+                # load .mat files
                 radarData[files] = loadmat(pre_path + participant + '/' + radar + '/' + pattern + '/' + fileRef[files])[radarRef[files]]
                 if files == 'time' or files == 'bin':
                     radarData[files] = radarData[files].flatten()
             
+            # indices of range_bins falling in the range
             binIdx = np.where(np.logical_and(radarData['bin'] > rangeRef[radar][0], radarData['bin'] < rangeRef[radar][1]))
             signal = [] 
+            # iterating through each scan
             iterations = np.shape(radarData['mag'])[0]
             for i in range(iterations):
+                # maximum amplitude at the focused range bins
                 maxVal = np.max(radarData['mag'][i, :][binIdx])
                 signal.append(maxVal)
             
             timeElapsed = radarData['time'][-1] - radarData['time'][0]
             sampleRate = timeElapsed/iterations
 
+            # trim signal
             signal = np.asarray(signal)
             trimStart = 0
             trimEnd = timeElapsed
@@ -58,17 +65,21 @@ for radar in ['Radar 1', 'Radar 2', 'Radar 3']:
                 signal = signal[iterStart: iterEnd + 1]
                 iterations = len(signal)
             
+            # normalizing the signal
             signal = np.subtract(signal, np.mean(signal))
 
+            # FFT with the hamming function
             signalFFT = fft(np.multiply(signal, np.hamming(iterations)))
             freqs = np.multiply(np.linspace(0.0, 1.0/(2.0*sampleRate), iterations//2), 60)
             signalFFT = 2.0/iterations * np.abs(signalFFT[0:iterations//2])
 
+            # corresponding frequency bins
             idx = np.where(freqs < 90)[0][-1]
             freqs = freqs[:idx]
             signalFFT = signalFFT[:idx]
             breathRate[rCnt, cCnt] = str(freqs[np.argmax(signalFFT)])
             
+            # plotting the signal and the FFT
             plt.figure()
             plt.subplot(211)
             plt.plot(np.linspace(trimStart, trimEnd,iterations), signal)
@@ -89,5 +100,6 @@ for radar in ['Radar 1', 'Radar 2', 'Radar 3']:
 
         cCnt = -1
 
+# summary of estimated breathing rates
 df = pd.DataFrame(breathRate, columns=['browsing', 'fetal_left', 'fetal_right', 'freefall', 'left_turned', 'right_turned', 'soldier'], index=['Radar 1 BR_st1', 'Radar 1 BR_st2', 'Radar 2 BR_st1', 'Radar 2 BR_st2', 'Radar 3 BR_st1', 'Radar 3 BR_st2'])
 print(df)
